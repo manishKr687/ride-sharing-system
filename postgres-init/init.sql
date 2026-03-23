17b08cad814ce8c68a0978f404c2547f1a35473d
manishkumar10071998-prog <mfresher687@gmail.com>
2026-03-23 15:24:08 +0530

    Fixed dockerfile and docker-compose file

---
+++ postgres-init/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE user_service;
+CREATE DATABASE driver_service;
+CREATE DATABASE ride_service;
+CREATE DATABASE payment_service;
+CREATE DATABASE billing_service;
+CREATE DATABASE notification_service;
